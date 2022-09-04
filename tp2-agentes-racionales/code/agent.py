ba43d555b7c7a63c486c4c2d67c5fdcf283eb1f4
juanm4morales <juanm4morales@gmail.com>
2022-09-04 17:32:11 -0300

    sin terminar

--- tp2-agentes-racionales/code/agent.py
+++ tp2-agentes-racionales/code/agent.py
@@ -5,4 +5,27 @@ class Agent:
         self.enviroment=enviroment
     
     def up(self):
-        self.enviroment.update_agent()
+        self.enviroment.accept_action((0,1), False)
+    
+    def down(self):
+        self.enviroment.accept_action((0,-1), False)
+        
+    def left(self):
+        self.enviroment.accept_action((-1,0), False)
+    
+    def right(self):
+        self.enviroment.accept_action((1,0), False)
+    
+    def suck(self):
+        self.enviroment.accept_action((0,0), True)
+    
+    def idle(self):
+        self.enviroment.accept_action((0,0), False)
+        
+    def perspective(self):
+        agentPosition=self.enviroment.agentPosition
+        dirtyCell=self.enviroment.is_dirty()
+        return (dirtyCell,agentPosition)
+    
+    #def think(self):
+        
--- tp2-agentes-racionales/code/enviroment.py
+++ tp2-agentes-racionales/code/enviroment.py
@@ -1,9 +1,10 @@
 import random
-
+ 
 class Cell:
     def __init__(self):
         self.dirty=False
         self.agent=False
+       
 class Enviroment:
     def __init__(self,sizeX,sizeY,init_posX,init_posY,dirt_rate):
         self.sizeX=sizeX
@@ -14,33 +15,97 @@ class Enviroment:
         self.currentPosY=init_posY
         self.dirt_rate=dirt_rate
         dirty_cells=dirt_rate*sizeX*sizeY
-        grid = make_grid(sizeX,sizeY)
-        self.grid = dirty_grid(grid, dirty_cells)
-        self.agentPosition=None
-                
-    def make_grid(sizeX,sizeY):
+        self.grid: list[list[Cell]] = self.make_grid(sizeX, sizeY)
+        self.dirty_grid(self.grid, dirty_cells)
+        self.grid[init_posY][init_posX].agent=True
+        self.agentPosition=[init_posX,init_posY]
+               
+    def make_grid(self,sizeX,sizeY):
         grid=[]
-        for i in range(sizeX):
+        for i in range(0,sizeY):
             grid.append([])
-            for j in range(sizeY):
+            for j in range(0,sizeX):
                 cell=Cell()                
                 grid[i].append(cell)
-            
         return grid
-    
-    def dirty_grid(grid, dirty_cells):
+   
+    def dirty_grid(self, grid : list[list[Cell]], dirty_cells):
         sizeX=len(grid)
         sizeY=len(grid[0])
         while (dirty_cells>0):
-            x=random.randint(0, sizeX)
-            y=random.randint(0, sizeY)
-            if (grid[x][y]!='d'):
-                grid[x][y]='d'
+            x=random.randint(0, sizeX-1)
+            y=random.randint(0, sizeY-1)
+            if not(grid[y][x].dirty):
+                grid[y][x].dirty=True
                 dirty_cells=dirty_cells-1
-
-    def update_agent():
-        return -1
-
-        
-            
+ 
+    def print_enviroment(self):
+        grid = self.grid
+        for i in range (0,self.sizeX):
+            print("|",end="")
+           
+            gridRow=grid[i]
+           
+            for j in range(0,self.sizeY):
+                gridCell=gridRow[j]
+                if gridCell.dirty:
+                    print("{",end="")
+                    if gridCell.agent:
+                        print("A",end="")
+                    else:
+                        print(" ",end="")
+                    print("}|",end="")
+                else:
+                    if gridCell.agent:
+                        print(" A |",end="")
+                    else:
+                        print("   |",end="")
+            print("")
+        print("")
+                       
+    
+    def accept_action(self,movement: (int,int),suck:bool):
+        if not(suck) and abs(movement[0]+movement[1])==1:
+            newX = movement[0] + self.agentPosition[0]
+            newY = movement[1] + self.agentPosition[1]
+            if (newX>=0 and newX<self.sizeX) and (newY>=0 and newY<self.sizeY):
+                self.move_agent(newX, newY)
+                return 0
+            else:
+                return -1
+        else:
+            self.grid[self.agentPosition[1]][self.agentPosition[0]].dirty=False
+            return 0
                 
+    def move_agent(self,x,y):
+        self.grid[self.agentPosition[1]][self.agentPosition[0]].agent=False
+        self.agentPosition[0]=x
+        self.agentPosition[1]=y
+        #print("x= " +str(x))
+        #print("y= " +str(y))
+        self.grid[y][x].agent=True
+        
+    def is_dirty(self):
+        return self.grid[self.agentPosition[1]][self.agentPosition[0]].dirty
+ 
+       
+env = Enviroment(10, 10, 0, 0, 0.8)
+
+env.printGrid()
+print(env.is_dirty())
+env.update_agent((1,0), False)
+env.update_agent((0,0), True)
+env.printGrid()
+env.update_agent((1,0), False)
+print(env.is_dirty())
+env.update_agent((0,0), True)
+env.printGrid()
+env.update_agent((1,0), False)
+print(env.is_dirty())
+env.update_agent((0,0), True)
+env.printGrid()
+env.update_agent((1,0), False)
+print(env.is_dirty())
+env.update_agent((0,0), True)
+env.printGrid()
+
