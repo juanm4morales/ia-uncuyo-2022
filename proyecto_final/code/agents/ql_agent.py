35c62893d95fcf136862bd521ec47ad15d6f4970
juanm4morales <juanm4morales@gmail.com>
2024-04-22 00:33:41 -0300

    Diseño de simulaciones

--- proyecto_final/code/agents/ql_agent.py
+++ proyecto_final/code/agents/ql_agent.py
@@ -1,12 +1,38 @@
 
+
+import numpy as np
+
 class QLAgent:
     
-    def __init__(self, enviroment, gamma, alpha):
+    def __init__(self, enviroment, gamma, alpha, startEpsilon, endEpsilon, decayRate):
         self.enviroment = enviroment
         self.lastReward = 0
         self.accReward = 0
         self.action = None
         self.gamma = gamma
         self.alpha = alpha
-        # self.qTable
+        self.startEpsilon = startEpsilon
+        self.endEpsilon = endEpsilon
+        self.decayRate = decayRate
+        
+    
+    def train(self, episodes):
+        for episode in range(episodes):
+            epsilon = self.endEpsilon + (self.startEpsilon - self.endEpsilon) * np.exp(-decayRate*episode)
+            enviroment.reset()
+            step = 0
+            done = False
+            
+            while (enviroment.finishedSimulation()):
+                action = epsilonGreedyPolicy(Qtable, state, epsilon)
+                
+                newState, reward, done, info = enviroment.step(action)
+                qTable[state][action] = qTable[state][action] + learningRate * (reward + gamma * np.max(qTable[newState]) - qtable[state][action])
+                
+                if done:
+                    break
+                    
+                state = newState
+                                    
+        return qTable
         
