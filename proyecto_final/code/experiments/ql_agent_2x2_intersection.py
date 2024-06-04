85365df227d9d0eeb58647a523cb6a8ae0d67053
juanm4morales <juanm4morales@gmail.com>
2024-06-04 02:02:11 -0300

    initial experiment added

--- proyecto_final/code/experiments/ql_agent_2x2_intersection.py
+++ proyecto_final/code/experiments/ql_agent_2x2_intersection.py
@@ -2,11 +2,13 @@ import sys
 import os
 
 current_dir=os.path.dirname(__file__)
-parent_dir=os.path.dirname(current_dir)
-sys.path.append(os.path.join(current_dir, '..'))
 
-from enviroments.environment import SumoEnvironment
-from agents.ql_agent import QLAgent
+sys.path.append(os.path.join(current_dir, '..'))
+print(sys.path)
+
+
+from tscRL.environments.environment import SumoEnvironment
+from tscRL.agents.ql_agent import QLAgent
 
 if "SUMO_HOME" in os.environ:
     tools = os.path.join(os.environ["SUMO_HOME"], "tools")
@@ -15,8 +17,9 @@ else:
     sys.exit("Please declare the environment variable 'SUMO_HOME'")
     
 
-env = SumoEnviroment("nets/2x2_intersection/interseccion.sumocfg", 5, 2, 4, 60, gui=True)
+env = SumoEnvironment("nets/2x2_intersection/interseccion.sumocfg", 5, 2, 4, 60, gui=True, edges=False, discreteIntervals=6, laneInfo="halted", rewardFn="diff_halted")
 
 agent = QLAgent(env, 0.99, 0.1, 0.05, 0.005, 1.0)
 
 agent.train(1)
+
